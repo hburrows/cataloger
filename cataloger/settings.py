3d95e577c24d545de495feb2233ac6a14b43e6c5
Howard Burrows <howard@Old-MacBook-Pro.local>
2013-05-07 09:16:54 -0700

    db config change back to mysql

--- cataloger/settings.py
+++ cataloger/settings.py
@@ -18,10 +18,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'catalogit',                      # Or path to database file if using sqlite3.
+        'NAME': 'cit',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'howard',
-        'PASSWORD': 'd#vel0p',
+        'USER': 'cit',
+        'PASSWORD': 'cit',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
